09389298bcc3004ac6917fc192f65826a19aca07
Aluno <Aluno@DOSO121791.educacional.up>
2018-06-19 10:55:55 -0300

    telas main,cadastrar,logar e menu inicial prontas e intents para ir para as telas login e cadastrar

--- app/src/main/java/com/example/ti/aplicativodietasaude/MainActivity.java
+++ app/src/main/java/com/example/ti/aplicativodietasaude/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.ti.aplicativodietasaude;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cadastrarse(View view){
+        Intent intent = new Intent(this, CadastroPageActivity.class);
+        startActivity(intent);
+    }
+
+    public void logarse(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
